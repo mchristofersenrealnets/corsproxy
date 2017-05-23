6723d87216ff695ab9be5357b9956617ec9e7ee8
Michael Christofersen <mchristofersen@realnets.com>
2017-05-23 13:14:01 -0500

    get token

--- lockstate.js
+++ lockstate.js
@@ -1,4 +1,10 @@
 let request = require("request");
+let async = require("async");
+
+const clientId= "6bd28423e3a931648a9484f930b214ce3881c712841b787b51b274986ec4bd47";
+const clientSecret = "a660efe1e5132dcd07500ec8b73135819f408b6d794bd356004ebaaed3ca54ef";
+const oAuthUrl = "https://connect.devicewebmanager.com/oauth/token?client_id=6bd28423e3a931648a9484f930b214ce3881c712841b787b51b274986ec4bd47&client_secret=a660efe1e5132dcd07500ec8b73135819f408b6d794bd356004ebaaed3ca54ef&grant_type=client_credentials";
+const lockstateUrl = "https://api.lockstate.com"
 
 function handleWebhook(body, done){
   if(body.status=="STOP"){
@@ -17,7 +23,16 @@ function cancelReservation(body, done){
 
 function sendConfirmation(body, done){
   console.log(`Sending Confirmation to ${body.customer.email}`);
-  done("success!");
+  getToken((response)=>{
+    console.log(response.access_token);
+    done("success!");
+  })
+}
+
+function getToken(done){
+  request({ url: oAuthUrl, method: "POST"},(error,response,body)=>{
+    done(body);
+  })
 }
 
 module.exports = handleWebhook;
